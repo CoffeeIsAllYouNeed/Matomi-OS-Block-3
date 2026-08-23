145b1abd3af6b260067bd33a837e679d0ea78e2d
CoffeeIsAllYouNeed <vrushalgithub2004@gmail.com>
2026-08-23 16:19:40 +0000

    feat: force transparent hugepages always-on by default for AI workload memory patterns

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -58,13 +58,9 @@
  * Defrag is invoked by khugepaged hugepage allocations and by page faults
  * for all hugepage allocations.
  */
+/* MatomiOS: force huge pages always-on by default (was conditional) */
 unsigned long transparent_hugepage_flags __read_mostly =
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE_ALWAYS
 	(1<<TRANSPARENT_HUGEPAGE_FLAG)|
-#endif
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE_MADVISE
-	(1<<TRANSPARENT_HUGEPAGE_REQ_MADV_FLAG)|
-#endif
 	(1<<TRANSPARENT_HUGEPAGE_DEFRAG_REQ_MADV_FLAG)|
 	(1<<TRANSPARENT_HUGEPAGE_DEFRAG_KHUGEPAGED_FLAG)|
 	(1<<TRANSPARENT_HUGEPAGE_USE_ZERO_PAGE_FLAG);
